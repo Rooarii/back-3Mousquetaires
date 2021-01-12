1364e2a772ffd24c0cdbfc00ddb199e89915ea56
Rooarii <rooarii.manuel@gmail.com>
2021-01-12 21:29:30 +0100

    add global down sql script for db init & seed

--- migrations/sqls/20210112202009-init-db-down.sql
+++ migrations/sqls/20210112202009-init-db-down.sql
@@ -1,1 +1,7 @@
 /* Replace with your SQL commands */
+
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- DROP TABLE IF EXISTS `table_name`
+
+SET FOREIGN_KEY_CHECKS = 1;
--- migrations/sqls/20210112202045-seed-db-down.sql
+++ migrations/sqls/20210112202045-seed-db-down.sql
@@ -1,1 +1,6 @@
 /* Replace with your SQL commands */
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- TRUNCATE TABLE `table_name`
+
+SET FOREIGN_KEY_CHECKS = 1;
